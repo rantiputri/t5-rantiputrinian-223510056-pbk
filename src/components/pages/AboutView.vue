0a81ef521c7d6dc6114ff3c9699174406258f19b
aldipa11 <tengkualdiva11@gmail.com>
2024-05-23 21:23:15 +0700

    tugas5

---
+++ src/components/pages/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute , useRouter } from 'vue-router'
+
+const router = use()
+const route = useRoute()
+
+const search = computed({
+    get() {
+        return route.query.search ?? ''
+    }
+})
+</script>
+
+<template>
+    <h2>AboutView</h2>
+    <label>
+        search: <input v-model.trim="search" maxlength="20">
+    </label>
+</template>
---
+++ src/components/pages/HomeView.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+    methods: {
+        gotoAbout() {
+            this.$router.push('/about')
+        },
+    },
+}
+</script>
+
+<template>
+    <h2>HomeView</h2>
+    <button @click="gotoAbout">Go To About</button>
+</template>
